64d752a6eef213327cfa2b874baa8833e71c120f
Joseph Nyaga <joseph.kinyua@student.moringaschool.com>
2021-06-14 01:38:22 +0300

    add a function validate

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// the form must be checked to make sure all the mandatory fields are filled in .It requires just a loop through each field in the form and check
+// for data.
+//  the data that is entered must be checked for correct form and value,Implement appropriate logic to test correctness of data.
+function validate() {
+
+}
