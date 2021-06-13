29aa5f92d09c11a22c57856c86402f093f3232be
Joseph Nyaga <joseph.kinyua@student.moringaschool.com>
2021-06-14 02:32:25 +0300

    connecting HTML and Js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 // the form must be checked to make sure all the mandatory fields are filled in .It requires just a loop through each field in the form and check
 // for data.
 //  the data that is entered must be checked for correct form and value,Implement appropriate logic to test correctness of data.
+
 function validate() {
     var genders = document.getElementsByName("gender");
+
     if (document.myForm.century.value >= 22 && document.myForm.century.value < 19) {
         console.log("Please provide a valid century");
         document.myForm.year.focus();
@@ -25,6 +27,7 @@ function validate() {
     } else {
         return true;
     }
+
 }
 // putting the male name,female name and the day values in an array
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
@@ -32,6 +35,7 @@ var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
 var weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 function calculateDay() {
+
     var CC = parseInt(document.getElementById("century").value);
     var YY = parseInt(document.getElementById("year").value);
     var MM = parseInt(document.getElementById("month").value);
@@ -86,6 +90,7 @@ function yourGender() {
             }
             break
         default:
+
     }
 }
 
