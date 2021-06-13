f93440b6ba69c925b742c0362ecf51e42967bfeb
Joseph Nyaga <joseph.kinyua@student.moringaschool.com>
2021-06-14 01:44:57 +0300

    add of if condition

--- js/script.js
+++ js/script.js
@@ -3,4 +3,9 @@
 //  the data that is entered must be checked for correct form and value,Implement appropriate logic to test correctness of data.
 function validate() {
     var genders = document.getElementsByName("gender");
+    if (document.myForm.century.value >= 22 && document.myForm.century.value < 19) {
+        console.log("Please provide a valid century");
+        document.myForm.year.focus();
+        return false;
+    }
 }
