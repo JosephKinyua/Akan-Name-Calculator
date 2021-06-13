69dd9f41e0e292cacf77c008e569740d372b3f0f
Joseph Nyaga <joseph.kinyua@student.moringaschool.com>
2021-06-14 01:49:06 +0300

    add three arrays male,female and days

--- js/script.js
+++ js/script.js
@@ -26,3 +26,6 @@ function validate() {
         return true;
     }
 }
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
+var weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
